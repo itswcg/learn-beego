63ad29634505a370c1aee3d128c645ac210743b5
itswcg <itswcg@gmail.com>
2019-03-28 18:07:31 +0800

    feat: chatroom

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/beego/i18n"
 	_ "learn-beego/routers"
 )
 
+const (
+	APP_VER = "1.0"
+)
+
 func main() {
+	beego.Info(beego.BConfig.AppName, APP_VER)
+
+	beego.AddFuncMap("i18n", i18n.Tr)
+
 	beego.Run()
 }
--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,13 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.AppController{})
+	beego.Router("/join", &controllers.AppController{}, "post:Join")
+
+	beego.Router("/lp", &controllers.LongPollingController{}, "get:Join")
+	beego.Router("/lp/post", &controllers.LongPollingController{})
+	beego.Router("/lp/fetch", &controllers.LongPollingController{}, "get:Fetch")
+
+	beego.Router("/ws", &controllers.WebSocketController{})
+	beego.Router("/ws/join", &controllers.WebSocketController{}, "get:Join")
 }
